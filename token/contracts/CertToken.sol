c4404fab810e31d5685d15fc97cd55321ccd13b3
Inaki <inaki.seco@opendeusto.es>
2018-03-27 14:21:49 +0200

    first functions

--- token/contracts/CertToken.sol
+++ token/contracts/CertToken.sol
@@ -13,17 +13,19 @@ contract CertToken {
     }
     
     struct Certificate {
-        address owner;          // Address of the certificate's owner
-        address issuer;         // Address of the entity who issues the certificate
+        address owner;          // Public address of the certificate's owner
+        address issuer;         // Public address of the entity who issues the certificate
         bytes32 certName;       // Name of the certificate issued
         address[] whiteList;    // List of authorized entities to check certificate
     }
 
-    Entity public newIssuer;
+    address public addr1;
+    uint public nounce;
+
+    mapping(bytes32 => Certificate) public certs;  // This creates an array with all the certificates
 
     mapping(address => User) public owners;
     mapping(address => Entity) public entities;
-    mapping (address => Certificate) public certificates;     // This creates an array with all the entities
      
     /********************************************Events******************************************/
 
@@ -36,7 +38,7 @@ contract CertToken {
     Initializes contract with initial supply tokens to the creator of the contract
     /********************************************************************************************/
     function CertToken() public {
-
+        nounce = 0;
     }
 
 
@@ -44,19 +46,38 @@ contract CertToken {
 
 
     /************************************Getters and Setters*************************************/
+    /********************************************************************************************
+    Set a new certificate to the blockcahin
 
+    newOwner        Address of new certificate's owner
+    newCertName     Name of the certificate
+    /********************************************************************************************/
+    function setCert(address newOwner, bytes32 newCertName) public returns (bytes32) {
+        bytes32 unique = keccak256(newOwner, nounce++);
 
+        certs[unique].owner = newOwner;
+        certs[unique].issuer = msg.sender;
+        certs[unique].certName = newCertName;
+        //certs[unique].whiteList[0] = newOwner;
+        
+        return unique;
+    }
 
 
 
 
 
     /*****************************************Functions*****************************************/
+    /********************************************************************************************
+    Check whether a certificate exist
 
-
-
-
-
-
+    certHash        Address of the certificate is gonna check
+    /********************************************************************************************/
+    function checkCert(bytes32 unique) public view returns (bool) {
+        if (certs[unique].issuer != 0 ) {
+            return true;
+        }
+        return false;
+    }
 
 }
