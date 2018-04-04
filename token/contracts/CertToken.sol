c5285c4e9e73eee5bc0c597b56588fa0a691ce5b
Inaki <inaki.seco@opendeusto.es>
2018-04-04 08:48:30 +0200

    some getters and setters added

--- token/contracts/CertToken.sol
+++ token/contracts/CertToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.13;
 contract CertToken {
     
     struct Certificate {
-        address owner;          // Public address of the certificate's owner
+        address owner;          // Public address of the certificate's owner (user or entity)
         address issuer;         // Public address of the entity who issues the certificate
         bytes32 certName;       // Name of the certificate issued
         address[] whiteList;    // List of authorized entities to check the certificate
@@ -17,7 +17,7 @@ contract CertToken {
 
     struct User {
         bytes32 name;           // Owner's name
-        bytes32 surnames;       // Owner's surnames
+        //bytes32 surnames;       // Owner's surnames
         bytes32 nid;            // Owner's national identity document
     }
 
@@ -26,7 +26,7 @@ contract CertToken {
 
     mapping(uint256 => Certificate) public certs;  // This creates an array with all the certificates
 
-    mapping(address => User) public owners;
+    mapping(address => User) public users;
     mapping(address => Entity) public entities;
     
     /********************************************Events******************************************/
@@ -45,32 +45,75 @@ contract CertToken {
         
     }
 
+
+
     /************************************Getters and Setters*************************************/
     /********************************************************************************************
-    Set a new certificate to the blockcahin
+    Create a new entity
 
-    newOwner        Address of new certificate's owner
-    newCertName     Name of the certificate
+    add             Addres of the new entity
+    entityName      Name of the new entity
     /********************************************************************************************/
-    function setCert(address _newOwner, bytes32 _newCertName) public returns (uint256 id) {
-        //bytes32 unique = keccak256(nounce++);     // TODO: In the future the id will be a hash, not a uint256
-        id = nounce++;
+    function setEntity(address add, bytes32 entityName) public {
+        entities[add].name = entityName;
+    }
 
-        certs[id].owner = _newOwner;                // Addidng information
-        certs[id].issuer = newIssuer;
-        certs[id].certName = _newCertName;
-        certs[id].stilValid = true;
-        setEntityToWhiteList(id, _newOwner);        // The owner is allowed to check his own certificate
-        setEntityToWhiteList(id, msg.sender);       // The issuer is allowed to check the certificate
-        
-        return id;
+    /********************************************************************************************
+    Create a new user
+
+    add             Addres of the new user
+    userName        Name of the new user
+    userNid         New user's National Identity Card number
+    /********************************************************************************************/
+    function setUser(address add, bytes32 userName, bytes32 userNid) public {
+        users[add].name = userName;
+        users[add].nid = userNid;
+    }
+
+    /********************************************************************************************
+    Get the name of an entity by its address
+
+    add             Address of the entity to be searched
+    /********************************************************************************************/
+    function getEntityByAddress(address add) constant public returns (bytes32) {
+        return entities[add].name;
+    }
+
+    /********************************************************************************************
+    Get the info of a user by his address
+
+    add             Address of the user to be searched
+    /********************************************************************************************/
+    function getUserByAddress(address add) constant public returns (bytes32, bytes32) {
+        return (users[add].name, users[add].nid);
     }
 
 
 
 
 
+
     /*****************************************Functions*****************************************/
+    /********************************************************************************************
+    Create a new certificate to the blockcahin
+
+    _to             Address of new certificate's owner
+    certName        Name of the new certificate
+    /********************************************************************************************/
+    function newCert(address _to, bytes32 _certName) public returns (uint256 id) {
+        //bytes32 unique = keccak256(nounce++);     // TODO: In the future the id will be a hash, not a uint256
+        id = nounce++;
+
+        certs[id].owner = _to;                // Addidng information
+        certs[id].issuer = newIssuer;
+        certs[id].certName = _certName;
+        certs[id].stilValid = true;
+        setEntityToWhiteList(id, _to);        // The owner is allowed to check his own certificate
+        setEntityToWhiteList(id, msg.sender);       // The issuer is allowed to check the certificate
+        
+        return id;
+    }
+    
     /********************************************************************************************
     Check whether a certificate exist
 
